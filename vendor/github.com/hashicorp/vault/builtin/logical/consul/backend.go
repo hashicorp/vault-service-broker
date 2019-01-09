e0b5c06b9b67fe8c4cd50b1ec2aceb3d6266f3e5
Becca Petrin <beccapetrin@posteo.net>
2019-01-08 16:40:11 -0800

    update dependencies (#35)

---
+++ vendor/github.com/hashicorp/vault/builtin/logical/consul/backend.go
@@ -0,0 +1,36 @@
+package consul
+
+import (
+	"github.com/hashicorp/vault/logical"
+	"github.com/hashicorp/vault/logical/framework"
+)
+
+func Factory(conf *logical.BackendConfig) (logical.Backend, error) {
+	b := Backend()
+	if err := b.Setup(conf); err != nil {
+		return nil, err
+	}
+	return b, nil
+}
+
+func Backend() *backend {
+	var b backend
+	b.Backend = &framework.Backend{
+		Paths: []*framework.Path{
+			pathConfigAccess(),
+			pathListRoles(&b),
+			pathRoles(),
+			pathToken(&b),
+		},
+
+		Secrets: []*framework.Secret{
+			secretToken(&b),
+		},
+	}
+
+	return &b
+}
+
+type backend struct {
+	*framework.Backend
+}
