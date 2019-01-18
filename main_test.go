1a7f6b783dd4e3ae0f6100c8a8337a2584aa3c7b
Becca Petrin <beccapetrin@posteo.net>
2019-01-18 08:44:38 -0800

    support storing env vars in credhub

--- main_test.go
+++ main_test.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"code.cloudfoundry.org/lager/lagertest"
 	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"testing"
 )
+
+var logger = lagertest.NewTestLogger("vault-broker-test")
 
 func TestNormalizeAddr(t *testing.T) {
 	cases := []struct {
@@ -66,7 +71,7 @@ func TestParseConfigDefaults(t *testing.T) {
 	os.Setenv("SECURITY_USER_PASSWORD", "buzz")
 	os.Setenv("VAULT_TOKEN", "bang")
 
-	config, err := parseConfig()
+	config, err := parseConfig(logger)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -132,7 +137,7 @@ func TestParseConfigFromEnv(t *testing.T) {
 	os.Setenv("SERVICE_TAGS", "hello,world")
 	os.Setenv("VAULT_RENEW", "false")
 
-	config, err := parseConfig()
+	config, err := parseConfig(logger)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -178,4 +183,229 @@ func TestParseConfigFromEnv(t *testing.T) {
 	if config.VaultRenew != false {
 		t.Fatal("expected false but received true")
 	}
+}
+
+func TestParseConfigFromCredhub(t *testing.T) {
+	os.Clearenv()
+
+	credhubTs := testCredhubServer()
+	defer credhubTs.Close()
+	os.Setenv("CREDHUB_URL", credhubTs.URL)
+
+	uaaTs := uaaTestServer()
+	defer uaaTs.Close()
+	os.Setenv("UAA_ENDPOINT", uaaTs.URL)
+	os.Setenv("UAA_CLIENT_NAME", "client-name")
+	os.Setenv("UAA_CLIENT_SECRET", "client-secret")
+
+	config, err := parseConfig(logger)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if config.SecurityUserName != "securityUserName" {
+		t.Fatalf("expected %s but received %s", `"securityUserName"`, config.SecurityUserName)
+	}
+	if config.SecurityUserPassword != "securityUserPassword" {
+		t.Fatalf("expected %s but received %s", `"securityUserPassword"`, config.SecurityUserPassword)
+	}
+	if config.VaultToken != "vaultToken" {
+		t.Fatalf("expected %s but received %s", `"vaultToken"`, config.VaultToken)
+	}
+	if config.CredhubURL != credhubTs.URL {
+		t.Fatalf("expected %s but received %s", credhubTs.URL, config.CredhubURL)
+	}
+	if config.UAAEndpoint != uaaTs.URL {
+		t.Fatalf("expected %s but received %s", uaaTs.URL, config.UAAEndpoint)
+	}
+	if config.UAAClientName != "client-name" {
+		t.Fatalf("expected %s but received %s", "client-name", config.UAAClientName)
+	}
+	if config.UAAClientSecret != "client-secret" {
+		t.Fatalf("expected %s but received %s", "client-secret", config.UAAClientSecret)
+	}
+	if config.Port != ":8080" {
+		t.Fatalf("expected %s but received %s", `":8080"`, config.Port)
+	}
+	if config.ServiceID != "serviceID" {
+		t.Fatalf("expected %s but received %s", `"serviceID"`, config.ServiceID)
+	}
+	if config.VaultAddr != "https://vaultAddr/" {
+		t.Fatalf("expected %s but received %s", `"http:s//vaultAddr/"`, config.VaultAddr)
+	}
+	if config.VaultAdvertiseAddr != "https://vaultAdvertiseAddr/" {
+		t.Fatalf("expected %s but received %s", `"https://vaultAdvertiseAddr/"`, config.VaultAdvertiseAddr)
+	}
+	if config.ServiceName != "serviceName" {
+		t.Fatalf("expected %s but received %s", `"serviceName"`, config.ServiceName)
+	}
+	if config.ServiceDescription != "serviceDescription" {
+		t.Fatalf("expected %s but received %s", `"serviceDescription"`, config.ServiceDescription)
+	}
+	if config.PlanName != "planName" {
+		t.Fatalf("expected %s but received %s", `"planName"`, config.PlanName)
+	}
+	if config.PlanDescription != "planDescription" {
+		t.Fatalf("expected %s but received %s", `"planDescription"`, config.PlanDescription)
+	}
+	if len(config.ServiceTags) != 2 {
+		t.Fatalf("expected %d but received %d: %s", 2, len(config.ServiceTags), config.ServiceTags)
+	}
+	if config.VaultRenew != false {
+		t.Fatal("expected false but received true")
+	}
+}
+
+func TestCredhubConfigOverridesEnvConfig(t *testing.T) {
+	os.Clearenv()
+
+	os.Setenv("SECURITY_USER_NAME", "fizz")
+	os.Setenv("SECURITY_USER_PASSWORD", "buzz")
+	os.Setenv("VAULT_TOKEN", "bang")
+
+	os.Setenv("PORT", "8080")
+	os.Setenv("SERVICE_ID", "1234")
+	os.Setenv("VAULT_ADDR", "http://localhost:8200")
+	os.Setenv("VAULT_ADVERTISE_ADDR", "https://some-domain.com")
+	os.Setenv("SERVICE_NAME", "vault")
+	os.Setenv("SERVICE_DESCRIPTION", "Vault, by Hashicorp")
+	os.Setenv("PLAN_NAME", "free")
+	os.Setenv("PLAN_DESCRIPTION", "Can you believe it's opensource?")
+	os.Setenv("SERVICE_TAGS", "hello,world")
+	os.Setenv("VAULT_RENEW", "false")
+
+	credhubTs := testCredhubServer()
+	defer credhubTs.Close()
+	os.Setenv("CREDHUB_URL", credhubTs.URL)
+
+	uaaTs := uaaTestServer()
+	defer uaaTs.Close()
+	os.Setenv("UAA_ENDPOINT", uaaTs.URL)
+	os.Setenv("UAA_CLIENT_NAME", "client-name")
+	os.Setenv("UAA_CLIENT_SECRET", "client-secret")
+
+	config, err := parseConfig(logger)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if config.SecurityUserName != "securityUserName" {
+		t.Fatalf("expected %s but received %s", `"securityUserName"`, config.SecurityUserName)
+	}
+	if config.SecurityUserPassword != "securityUserPassword" {
+		t.Fatalf("expected %s but received %s", `"securityUserPassword"`, config.SecurityUserPassword)
+	}
+	if config.VaultToken != "vaultToken" {
+		t.Fatalf("expected %s but received %s", `"vaultToken"`, config.VaultToken)
+	}
+	if config.CredhubURL != credhubTs.URL {
+		t.Fatalf("expected %s but received %s", credhubTs.URL, config.CredhubURL)
+	}
+	if config.UAAEndpoint != uaaTs.URL {
+		t.Fatalf("expected %s but received %s", uaaTs.URL, config.UAAEndpoint)
+	}
+	if config.UAAClientName != "client-name" {
+		t.Fatalf("expected %s but received %s", "client-name", config.UAAClientName)
+	}
+	if config.UAAClientSecret != "client-secret" {
+		t.Fatalf("expected %s but received %s", "client-secret", config.UAAClientSecret)
+	}
+	if config.Port != ":8080" {
+		t.Fatalf("expected %s but received %s", `":8080"`, config.Port)
+	}
+	if config.ServiceID != "serviceID" {
+		t.Fatalf("expected %s but received %s", `"serviceID"`, config.ServiceID)
+	}
+	if config.VaultAddr != "https://vaultAddr/" {
+		t.Fatalf("expected %s but received %s", `"http:s//vaultAddr/"`, config.VaultAddr)
+	}
+	if config.VaultAdvertiseAddr != "https://vaultAdvertiseAddr/" {
+		t.Fatalf("expected %s but received %s", `"https://vaultAdvertiseAddr/"`, config.VaultAdvertiseAddr)
+	}
+	if config.ServiceName != "serviceName" {
+		t.Fatalf("expected %s but received %s", `"serviceName"`, config.ServiceName)
+	}
+	if config.ServiceDescription != "serviceDescription" {
+		t.Fatalf("expected %s but received %s", `"serviceDescription"`, config.ServiceDescription)
+	}
+	if config.PlanName != "planName" {
+		t.Fatalf("expected %s but received %s", `"planName"`, config.PlanName)
+	}
+	if config.PlanDescription != "planDescription" {
+		t.Fatalf("expected %s but received %s", `"planDescription"`, config.PlanDescription)
+	}
+	if len(config.ServiceTags) != 2 {
+		t.Fatalf("expected %d but received %d: %s", 2, len(config.ServiceTags), config.ServiceTags)
+	}
+	if config.VaultRenew != false {
+		t.Fatal("expected false but received true")
+	}
+}
+
+func testCredhubServer() *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
+		if request.URL.Path != "/api/v1/data" {
+			writer.WriteHeader(400)
+			writer.Write([]byte(fmt.Sprintf("unrecognized path: %s", request.URL.Path)))
+			return
+		}
+		respVal := ""
+		switch request.URL.Query().Get("name") {
+		case "VAULT_SERVICE_BROKER_SECURITY_USER_NAME":
+			respVal = "securityUserName"
+		case "VAULT_SERVICE_BROKER_SECURITY_USER_PASSWORD":
+			respVal = "securityUserPassword"
+		case "VAULT_SERVICE_BROKER_VAULT_TOKEN":
+			respVal = "vaultToken"
+		case "VAULT_SERVICE_BROKER_PORT":
+			respVal = "8080"
+		case "VAULT_SERVICE_BROKER_SERVICE_ID":
+			respVal = "serviceID"
+		case "VAULT_SERVICE_BROKER_VAULT_ADDR":
+			respVal = "vaultAddr"
+		case "VAULT_SERVICE_BROKER_VAULT_ADVERTISE_ADDR":
+			respVal = "vaultAdvertiseAddr"
+		case "VAULT_SERVICE_BROKER_SERVICE_NAME":
+			respVal = "serviceName"
+		case "VAULT_SERVICE_BROKER_SERVICE_DESCRIPTION":
+			respVal = "serviceDescription"
+		case "VAULT_SERVICE_BROKER_PLAN_NAME":
+			respVal = "planName"
+		case "VAULT_SERVICE_BROKER_PLAN_DESCRIPTION":
+			respVal = "planDescription"
+		case "VAULT_SERVICE_BROKER_SERVICE_TAGS":
+			respVal = "service,tags"
+		case "VAULT_SERVICE_BROKER_VAULT_RENEW":
+			respVal = "false"
+		default:
+			writer.WriteHeader(400)
+		}
+		respBody := fmt.Sprintf(`{
+			"data": [{
+				"type": "password",
+				"version_created_at": "2017-01-05T01:01:01Z",
+				"id": "2993f622-cb1e-4e00-a267-4b23c273bf3d",
+				"name": "/example-password",
+				"value": "%s"
+			}]
+		}`, respVal)
+
+		writer.WriteHeader(200)
+		writer.Write([]byte(respBody))
+	}))
+}
+
+func uaaTestServer() *httptest.Server {
+	return httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.URL.Path {
+		case "/oauth/token":
+			fmt.Fprintf(w, `{
+			  "access_token" : "8d952f1311c041d19253fc01c2145144",
+			  "token_type" : "bearer",
+			  "id_token" : "eyJhbGciOiJIUzI1NiIsImprdSI6Imh0dHBzOi8vbG9jYWxob3N0OjgwODAvdWFhL3Rva2VuX2tleXMiLCJraWQiOiJsZWdhY3ktdG9rZW4ta2V5IiwidHlwIjoiSldUIn0.eyJzdWIiOiJjMWJhZTk2OC1hMjFlLTQ5ODItOGQwYi03ODJjMjQwNGI3OWYiLCJhdWQiOlsibG9naW4iXSwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgwL3VhYS9vYXV0aC90b2tlbiIsImV4cCI6MTU0NTQ3NjcwNSwiaWF0IjoxNTQ1NDMzNTA1LCJhenAiOiJsb2dpbiIsInNjb3BlIjpbIm9wZW5pZCJdLCJlbWFpbCI6IkQ3a1J6RkB0ZXN0Lm9yZyIsInppZCI6InVhYSIsIm9yaWdpbiI6InVhYSIsImp0aSI6IjhkOTUyZjEzMTFjMDQxZDE5MjUzZmMwMWMyMTQ1MTQ0IiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImNsaWVudF9pZCI6ImxvZ2luIiwiY2lkIjoibG9naW4iLCJncmFudF90eXBlIjoiYXV0aG9yaXphdGlvbl9jb2RlIiwidXNlcl9uYW1lIjoiRDdrUnpGQHRlc3Qub3JnIiwicmV2X3NpZyI6IjRkOWQ4ZjY5IiwidXNlcl9pZCI6ImMxYmFlOTY4LWEyMWUtNDk4Mi04ZDBiLTc4MmMyNDA0Yjc5ZiIsImF1dGhfdGltZSI6MTU0NTQzMzUwNX0.DDqZtEIaTgtIhT0iaRyEoNvDpsGvHuUMyxOS9Zo5fhI",
+			  "refresh_token" : "331e025fe0384bf588fae5bba0b7f784-r",
+			  "expires_in" : 43199,
+			  "scope" : "openid oauth.approvals",
+			  "jti" : "8d952f1311c041d19253fc01c2145144"
+			}`)
+		}
+	}))
 }
